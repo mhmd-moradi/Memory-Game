f77c30708b27c5852f12bb54cafc30ffcea50016
Mohamad Muradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-23 06:08:08 +0300

    JS: Added Global Variables

---
+++ game.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+
+    const blocks = Array.from(document.getElementsByClassName("block"));
+    const title = document.getElementById("title");
+    const body = document.getElementsByTagName("body")[0];
+
+    const colors = ["green", "red", "yellow", "blue"]; //game colors
+    var game_pattern = [];  //storing the game pattern   
+    var user_pattern = [];  //storing the pattern entered by the user for each level
+
+    var playing = false;    //to prevent body and blocks event listeners from occuring twice on each click
+    var current_level = 1;
+    var current_index = 0;  //to validate the clicked block by user on each click
+
+}
